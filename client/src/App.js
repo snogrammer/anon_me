c24f2fb776d6aa201d4cbf90e0e237e5e2463924
khaos <dev.ikhaos@gmail.com>
2018-08-03 09:02:03 -0600

    initial client implementation

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,81 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import { Container, Header, Segment, Button, Icon, Dimmer, Loader, Divider } from 'semantic-ui-react';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {};
+    this.getDigital = this.getDigital.bind(this);
+    // this.getPhysical = this.getPhysical.bind(this);
+  }
+
+  componentDidMount(){
+    this.getDigital();
+  }
+
+  fetch(endpoint){
+    return window.fetch(endpoint)
+      .then(response => response.json())
+      .catch(error => console.log(error))
+  }
+
+  getDigital(){
+    this.fetch('/api/info/digital')
+      .then(digital => this.setState({digital: digital}));
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    let { digital } = this.state;
+
+    return digital
+      ? <Container text textAlign='center'>
+          <Header as='h1' icon>
+            <Icon name='spy' circular />
+            <Header.Content>
+              Anonymize Me
+            </Header.Content>
+          </Header>
+
+          <Header as='h5'>
+            Protect your privacy and take back control of your data
+          </Header>
+
+          <Divider hidden section />
+
+          <Container>
+            <Header as='h3' icon>
+              <Icon name='user' />
+              <Header.Content>
+                {digital.username}
+              </Header.Content>
+            </Header>
+          </Container>
+
+          <Container>
+            <Header as='h3' icon href={digital.href}>
+              <Icon name='key' />
+              <Header.Content>
+                {digital.password}
+              </Header.Content>
+            </Header>
+          </Container>
+
+          <Container>
+            <Header as='h3' icon>
+              <Icon name='mail' />
+              <Header.Content>
+                {digital.email}
+              </Header.Content>
+            </Header>
+          </Container>
+
+        </Container>
+        : <Container text>
+            <Dimmer active inverted>
+              <Loader content='Loading' />
+            </Dimmer>
+          </Container>
   }
 }
 
